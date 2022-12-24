b219ef4df69922064121fcfa4a615108ea1914be
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2022-12-24 14:27:10 +0200

    Task 1 Done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,32 @@
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
-const body = document.body;
+const bodyColorValue = document.body;
+let timerID = null;
 
-
+btnStart.addEventListener('click', bodyChangeColor);
+btnStop.addEventListener('click', stopBodyChangeColor);
+btnStop.disabled = true;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
+
+ 
+function bodyChangeColor() {
+  
+    timerID = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        bodyColorValue.style.backgroundColor = randomColor;
+    }, 1000);
+
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+};
+
+function stopBodyChangeColor() {
+    clearInterval(timerID);
+
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+};
+
