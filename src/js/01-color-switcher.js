52f10714247cbeff32c13f56e877e6adc7f3053f
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2022-12-24 12:55:54 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,3 +3,7 @@ const btnStop = document.querySelector('[data-stop]');
 const body = document.body;
 
 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
