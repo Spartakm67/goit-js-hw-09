687463606232d9cdf5830e3612ab9931f2e1fd4f
Spartakm67 <109909363+Spartakm67@users.noreply.github.com>
2022-12-24 12:27:32 +0000

    Deploying to gh-pages from @ Spartakm67/goit-js-hw-09@b219ef4df69922064121fcfa4a615108ea1914be 🚀

---
+++ 01-color-switcher.0b2c6492.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body,d=null;t.addEventListener("click",(function(){d=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));a.style.backgroundColor=t}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0})),e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.0b2c6492.js.map
